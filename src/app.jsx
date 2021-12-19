b9c540840d08debe658d7085bdfc7a1c041a2689
한결같음 <83993373+ocxh@users.noreply.github.com>
2021-12-19 21:45:12 +0900

    Complete Search

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,30 @@
 
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import Search_header from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) {
     const [videos, setvideos] = useState([]);
+    const search = query => {
+            youtube
+            .search(query)
+            .then(videos => setvideos(videos));
+        };
+    
 
     useEffect(() => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-
-        fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCvy1wJYY6bxPb0AxdcbTmk7bWn1L0TKnY", 
-        requestOptions)
-            .then(response => response.json())
-            .then(result => setvideos(result.items))
-            .catch(error => console.log('error', error));
+            youtube
+            .mostPopular()
+            .then(videos => setvideos(videos));
     }, [])
-    return <VideoList videos={videos} />;
+    return (
+        <div className={styles.app}>
+            <Search_header onSearch={search}/>
+            <VideoList videos={videos} />          
+        </div>
+
+    )
 }
 
 export default App;
