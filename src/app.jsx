f752c01291b90340f079793d60c3c709998ca39e
한결같음 <83993373+ocxh@users.noreply.github.com>
2021-12-20 13:57:26 +0900

    back to the list the searching

--- src/app.jsx
+++ src/app.jsx
@@ -2,17 +2,25 @@
 import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import Search_header from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
 function App({youtube}) {
     const [videos, setvideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const selectVideo = (video) =>{
+        setSelectedVideo(video);
+    }
     const search = query => {
             youtube
             .search(query)
-            .then(videos => setvideos(videos));
-        };
+            .then(videos => {
+            setvideos(videos)
+            setSelectedVideo(null);
+            });
+    };
     
-
     useEffect(() => {
             youtube
             .mostPopular()
@@ -21,7 +29,20 @@ function App({youtube}) {
     return (
         <div className={styles.app}>
             <Search_header onSearch={search}/>
-            <VideoList videos={videos} />          
+            <section className={styles.content}>
+            { selectedVideo && (
+                <div className={styles.detail}>
+                    <VideoDetail video={selectedVideo} /> 
+                </div>
+            )}   
+                <div className={styles.list}>
+                    <VideoList 
+                    videos={videos} 
+                    onVideoClick={selectVideo} 
+                    display={selectedVideo ? 'list' : 'grid'}
+                    />
+                </div> 
+            </section>          
         </div>
 
     )
